f043795c1a61d8b1ccc954de4fa90aeaf17b9f85
Pavel Ershov <devnulpavel@gmail.com>
2018-03-12 22:03:56 +0300

    New windows code

--- Example_1/src/main.cpp
+++ Example_1/src/main.cpp
@@ -2,9 +2,12 @@
 #include "CommonDefines.h"
 #include "VulkanInstance.h"
 
+#include <windows.h>
+
 #define STRINGIFY(_STR_) (#_STR_)
 
-int main(int argc, char** argv) {
+
+int __main(int argc, char** argv) {
     glfwInit();
     
     if (glfwVulkanSupported()){
@@ -14,5 +17,17 @@ int main(int argc, char** argv) {
     //PFN_vkCreateInstance pfnCreateInstance = (PFN_vkCreateInstance)glfwGetInstanceProcAddress(NULL, "vkCreateInstance");
     
     return 0;
+}
+
+INT WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
+	printf("Windows start");
+	__main(1, NULL);
+	fflush(stdout);
+	if (glfwVulkanSupported()) {
+		printf("Vulkan available!!!");
+		MessageBox(NULL, lpCmdLine, "Vulkan available!!!", 0);
+	}
+	MessageBox(NULL, lpCmdLine, "WinMain Demo", 0);
+	return 0;
 }
 
